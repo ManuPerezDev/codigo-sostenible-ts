aaae1327167bb469ace33ee5415bec6ebc15e162
Manuel Pérez <manuel.perez@collaborator.audiense.com>
2023-03-10 15:52:15 +0000

    add more cases to string calculator

--- src/stringCalculator/StringCalculator.test.ts
+++ src/stringCalculator/StringCalculator.test.ts
@@ -3,7 +3,11 @@
  * '1' => 1; '2' => 2
  * '1,2' => 3; '1,2,3' => 6
  * '1,2,a,3' => 6
- * //;/1;2;3 => 6; //·/1·2·3 => 6
+ * '//;/1;2;3' => 6; //·/1·2·3 => 6
+ * '1\n2,3' => 6
+ * '1,-2,-3' // error: negatives not allowed: -2 -3
+ * '1,2000,3' => 4
+ * More use cases: https://www.codurance.com/katalyst/string-calculator
  */
 
 class StringCalculator {
@@ -12,13 +16,15 @@ class StringCalculator {
 
     if(this.hasCustomSeparator(expression)) {
       const matchedExpression = expression.match(/^\/\/(.*)\/(.*)/)
-      const separator = matchedExpression[1]
+      const customSeparator = matchedExpression[1]
       const onlyNumbersExpression = matchedExpression[2]
-      const numbers = this.getNumbersFrom(onlyNumbersExpression, separator);
+      const numbers = this.getNumbersFrom(onlyNumbersExpression, customSeparator);
+      this.checkNegative(numbers)
       return this.sum(numbers);
     }
 
     const numbers = this.getNumbersFrom(expression);
+    this.checkNegative(numbers)
     return this.sum(numbers);
   }
 
@@ -26,39 +32,73 @@ class StringCalculator {
     return expression.includes('/');
   }
 
-  private static sum(numbers: string[]) {
-    return numbers.reduce((acc, curr) => acc + Number(curr), 0);
+  private static sum(numbers: number[]) {
+    return numbers.reduce((acc, curr) => acc + curr, 0);
   }
 
-  private static getNumbersFrom(onlyNumbersExpression: string, separator: string = ',') {
-    return onlyNumbersExpression.split(separator).filter(char => Number(char));
+  private static getNumbersFrom(onlyNumbersExpression: string, customSeparator?: string) {
+    const separator = customSeparator ? customSeparator : /[\n,]/g
+    return onlyNumbersExpression.split(separator)
+      .filter(Number)
+      .map(Number)
+      .filter(number => number < 1000);
+  }
+
+  private static checkNegative(numbers: number[]) {
+    const negativeNumbers = numbers.reduce((acc, curr) => {
+      if(curr < 0) {
+        return [...acc, curr]
+      }
+      return acc
+    }, [])
+
+    if(negativeNumbers.length > 0) {
+      const stringifiedNegativeNumbers = negativeNumbers.join(' ')
+      throw new Error(`negatives not allowed: ${stringifiedNegativeNumbers}`)
+    }
   }
 }
 
 describe('StringCalculator should', () => {
-  it('should handle invalid values',  () => {
+  it('handle invalid values',  () => {
     expect(StringCalculator.add(null)).toBe(0);
     expect(StringCalculator.add(undefined)).toBe(0);
     expect(StringCalculator.add('')).toBe(0);
   });
 
-  it('should handle single values', () => {
+  it('handle single values', () => {
     expect(StringCalculator.add('1')).toBe(1);
     expect(StringCalculator.add('2')).toBe(2);
   });
 
-  it('should sum values',  () => {
+  it('sum values',  () => {
     expect(StringCalculator.add('1,2')).toBe(3);
     expect(StringCalculator.add('1,2,3')).toBe(6);
   });
 
-  it('should sum values excluding non number characters',  () => {
+  it('sum values excluding non number characters',  () => {
     expect(StringCalculator.add('1,2,y')).toBe(3);
     expect(StringCalculator.add('1,2,a,3')).toBe(6);
+    expect(StringCalculator.add('1a,2,a,3')).toBe(5);
   });
 
-  it('should accept a custom separator',  () => {
+  it('accept a custom separator',  () => {
     expect(StringCalculator.add('//·/1·2·3')).toBe(6);
     expect(StringCalculator.add('//;/1;2;3')).toBe(6);
+    expect(StringCalculator.add('//&/1;2;3')).toBe(0);
+  });
+
+  it('accept a line break as default separator',  () => {
+    expect(StringCalculator.add('1\n2,3')).toBe(6);
+  });
+
+  it('does not accepts negative numbers',  () => {
+    expect(() => StringCalculator.add('1,-2,-3')).toThrowError(new Error('negatives not allowed: -2 -3'));
+    expect(() => StringCalculator.add('//%/1%-3%-4')).toThrowError(new Error('negatives not allowed: -3 -4'));
+  });
+
+  it('ignore numbers greater than 1000',  () => {
+    expect(StringCalculator.add('1,2000,3')).toBe(4);
+    expect(StringCalculator.add('1001,2,3')).toBe(5);
   });
 });

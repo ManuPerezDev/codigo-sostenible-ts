a41edee14dfe141ff0e57b928daabdcd7ca53249
Manuel Pérez <manuel.perez@collaborator.audiense.com>
2023-04-21 13:54:48 +0100

    refactor(TemplateEngine): extract logic

--- src/templateEngine/src/TemplateEngine.ts
+++ src/templateEngine/src/TemplateEngine.ts
@@ -7,16 +7,21 @@ export class TemplateEngine {
     const template = Template.createValidTemplate(rawTemplate)
     const dictionary = Dictionary.createValidDictionary(rawDictionary)
 
-    let filledTemplate = template.value
-    for (const key in dictionary.value) {
-      filledTemplate = filledTemplate.replaceAll('${' + key + '}', dictionary.getValue(key))
-    }
+    let filledTemplate = this.fillTemplate(template, dictionary);
 
     if (this.thereAreRemainingMarkedText(filledTemplate)) {
       throw new MissingDictionaryKeyError()
     }
 
     return filledTemplate
+  }
+
+  private static fillTemplate(template: Template, dictionary: Dictionary) {
+    let filledTemplate = template.value
+    for (const key in dictionary.value) {
+      filledTemplate = filledTemplate.replaceAll('${' + key + '}', dictionary.getValue(key))
+    }
+    return filledTemplate;
   }
 
   private static thereAreRemainingMarkedText(filledTemplate: string) {
